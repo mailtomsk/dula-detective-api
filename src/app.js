6e734433a3905077b84e19ca0451e69206f41755
Sundara Pandian <sundar@oclocksolutions.com>
2025-07-30 14:37:10 +0000

    added cross origin site

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5000', 'http://localhost:5173'],
+    origin: ['http://localhost:5000', 'http://localhost:5173', 'https://duladetective.yourhostingnow.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-api-key'],
     credentials: true,
